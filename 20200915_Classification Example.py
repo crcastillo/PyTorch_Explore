db05fea27b3d4b6f153e3e75278fbe8e220895f9
crcastillo <crcastillo84@gmail.com>
2020-09-29 16:05:05 -0700

    Got PyTorch Lightning model with Optuna hyperparameter tuning to work

--- 20200915_Classification Example.py
+++ 20200915_Classification Example.py
@@ -27,6 +27,7 @@ import optuna
 import multiprocessing
 import importlib
 import datetime
+import os
 
 import plotly.express as px
 # from datetime import datetime
@@ -96,15 +97,15 @@ x_train = PreProcessing.fit_transform(X = X_Train_Split).astype('float32')
 x_valid = PreProcessing.transform(X = X_Valid_Split).astype('float32')
 x_test = PreProcessing.transform(X = X_Test).astype('float32')
 
-# Define training data class
-class trainData(torch.utils.data.Dataset):
-    def __init__(self, x_train, y_train):
-        self.x_train = x_train
-        self.y_train = y_train
-    def __getitem__(self, index):
-        return self.x_train[index], self.y_train[index]
-    def __len__(self):
-        return len(self.x_train)
+# # Define training data class
+# class trainData(torch.utils.data.Dataset):
+#     def __init__(self, x_train, y_train):
+#         self.x_train = x_train
+#         self.y_train = y_train
+#     def __getitem__(self, index):
+#         return self.x_train[index], self.y_train[index]
+#     def __len__(self):
+#         return len(self.x_train)
 
 # Define Data_Transform class to use in DataLoader
 class Data_Transform(torch.utils.data.Dataset):
@@ -351,14 +352,15 @@ NN_Lit_Model_trained.freeze()
 # TODO: Figure out the source of the illegal memory access error
 
 # Make sure we can get a prediction out | success
-NN_Lit_Model_trained(torch.randn(42).to(Device))
+NN_Lit_Model_trained.to('cpu')
+NN_Lit_Model_trained(torch.randn(42))
 
 # Score Test data
 Pred = np.array([])
 Actual = np.array([])
 for _inputs, _targets in test_dl:
     # Load inputs to GPU
-    _inputs_t = _inputs.to(Device)
+    _inputs_t = _inputs.to('cpu')
     # Compute model output
     _yhat = NN_Lit_Model_trained(_inputs_t)
     # Append predictions and targets
@@ -368,148 +370,135 @@ for _inputs, _targets in test_dl:
 print('AUC = {:.4f}'.format(roc_auc_score(y_true=Actual, y_score=Pred)))
 print('Average Precision Score = {:.4f}'.format(average_precision_score(y_true=Actual, y_score=Pred)))
 
+# AUC = 0.9290
+# Average Precision Score = 0.6131
+
 # </editor-fold>
 
-# <editor-fold desc="Use Optuna to hypertune the LogisticHazard model and create predictions">
+# <editor-fold desc="Use Optuna to hypertune the NeuralNet_Lit model and create predictions">
 
+# Construct the neural net class
+class NNet(torch.nn.Module):
+    def __init__(self, trial):
+        super(NNet, self).__init__()
+        self.layers = []
+        self.dropouts = []
 
+        # Optimize the number of hidden layers, hidden units, and dropout percentages
+        n_hidden_layers = trial.suggest_int('n_hidden_layers', 1, 3)
+        input_features = x_train.shape[1]
+        output_features_max = input_features * 2 - 1
 
-# Simplest build, likely best
-# Can't seem to remove duplicated unnecessary weights file...
-# Create Callback class to attempt end of epoch trial.prune
-class Callback_Prune(tt.callbacks.Callback):
-    def __init__(self, trial: optuna.trial.Trial, metric='loss'):
-        self.trial = trial
-        self.epoch = -1
-        self.metric = metric
+        # Append hidden layers and dropouts
+        for _i in range(n_hidden_layers):
+            # Store potential out_features
+            output_features = trial.suggest_int('n_units_l{}'.format(_i), low=4, high=output_features_max, log=True)
+            # Append a hidden layer
+            self.layers.append(torch.nn.Linear(in_features=input_features, out_features=output_features))
+            # Suggest a proportion for Dropout
+            p = trial.suggest_uniform("dropout_l{}".format(_i), 0.05, 0.5)
+            # Dropout layer, which randomly zeroes some proportion of the elements
+            self.dropouts.append(torch.nn.Dropout(p=p))
+            # Set input_features accordingly so any new layers take on the input dimension from the last output dimension
+            input_features = output_features
+        # Append final layer
+        self.layers.append(torch.nn.Linear(in_features=input_features, out_features=1))
+        # Assigning the layers as class variables (PyTorch requirement)
+        for idx, layer in enumerate(self.layers):
+            setattr(self, "fc{}".format(idx), layer)
+        # Assigning the dropouts as class variables (PyTorch requirement)
+        for idx, dropout in enumerate(self.dropouts):
+            setattr(self, "drop{}".format(idx), dropout)
+    # Define the forward pass
+    def forward(self, x):
+        for layer, dropout in zip(self.layers, self.dropouts):
+            x = torch.relu(layer(x))
+            x = dropout(x)
+        return torch.sigmoid(self.layers[-1](x))
 
-    def on_epoch_end(self):
-        # Advance _epoch by 1
-        self.epoch += 1
-        # Retrieve the current val score
-        self.score = self.model.log.monitors['val_'].scores[self.metric]['score'][-1]
-        # Report score to trial
-        self.trial.report(value=self.score, step=self.epoch)
-        # Check whether should prune
-        self.should_prune = self.trial.should_prune()
-        # Run pruning boolean
-        if self.should_prune:
-            # print(self.file_path)
-            message = 'Trial was pruned at iteration {}.'.format(self.epoch)
-            raise optuna.TrialPruned(message)
-        return self.should_prune
+# Create the PyTorch Lightning class
+class NNet_Lightning(pl.LightningModule):
+    def __init__(self, trial):
+        super(NNet_Lightning, self).__init__()
+        self.model = NNet(trial)
+        # Suggest different learning rates
+        self.lr = trial.suggest_loguniform('lr', 1e-6, 1e-1)
+    # Define the forward pass output
+    def forward(self, x):
+        return self.model(x)
+    # Define optimizer
+    def configure_optimizers(self):
+        return torch.optim.SGD(params=self.model.parameters(),lr=self.lr)
+    # Define the training step
+    def training_step(self, batch, batch_nb):
+        x, y = batch
+        loss = torch.nn.functional.binary_cross_entropy(input=self.forward(x), target=y)
+        return {'loss': loss}
+    # Define the validation step
+    def validation_step(self, batch, batch_nb):
+        x, y = batch
+        loss = torch.nn.functional.binary_cross_entropy(input=self.forward(x), target=y)
+        return {'batch_val_loss': loss}
+    # Define the validation post epoch end step
+    def validation_epoch_end(self, outputs):
+        avg_loss = sum(x["batch_val_loss"] for x in outputs) / len(outputs)
+        return {"log": {"avg_val_loss": avg_loss}}
 
-
-# Define function that will optimize hidden layers, in/out features, dropout ratio
-def Define_Model(trial: optuna.trial.Trial):
-
-    # Hidden layers between 1 and 5
-    n_hidden_layers = trial.suggest_int('n_hidden_layers', 1, 4)
-
-    # Instantiate list for layers
-    hidden_layers = []
-
-    # Define starting point for input features
-    input_features = X_Train_Split.shape[1]
-
-    # Iterate through hidden layer options
-    for _i in range(n_hidden_layers):
-
-        # Store potential out_features
-        out_features = trial.suggest_int('n_units_l{}'.format(_i), 4, 112)
-
-        # Append the layer
-        hidden_layers.append(
-            torch.nn.modules.linear.Linear(
-                in_features = input_features
-                , out_features = out_features
-            )
-        )
-        # Append the activation function
-        hidden_layers.append(torch.nn.modules.activation.ReLU())
-        # Append the batch normalization
-        hidden_layers.append(torch.nn.modules.batchnorm.BatchNorm1d(num_features = out_features))
-        # Suggest p probability for Dropout layer
-        p = trial.suggest_uniform("dropout_l{}".format(_i), 0.05, 0.5)
-        # Dropout layer, which randomly zeroes some proportion of the elements
-        hidden_layers.append(torch.nn.modules.dropout.Dropout(p = p))
-
-        # Set input_features accordingly so any new layers take on the input dimension from the last output dimension
-        input_features = out_features
-
-    # Append the final layer
-    hidden_layers.append(
-        torch.nn.modules.linear.Linear(
-            in_features = input_features
-            , out_features = Label_Transform.out_features
-        )
-    )
-
-    # Return back the Sequential Neural Network
-    return torch.nn.Sequential(*hidden_layers)
-
+# Create PyTorch Lightning metric callback
+class MetricsCallback(pl.Callback):
+    def __init__(self):
+        super().__init__()
+        self.metrics = []
+    def on_validation_end(self, trainer, pl_module):
+        self.metrics.append(trainer.callback_metrics)
 
 # Define objective function to optimize within Optuna study
-def LogHazard_Objective(trial: optuna.trial.Trial):
+def NNet_Objective(trial):
+    # Create trial model checkpoint
+    TrialCheckpointCallback = pl.callbacks.ModelCheckpoint(
+        filepath=os.path.join(
+            'Classifier Studies/'
+            ,'trial_{}'.format(trial.number)
+            , '_{epoch}'
+        )
+        , monitor='avg_val_loss'
+    )
+    # Instantiate the MetricsCallback
+    Metrics_Callback = MetricsCallback()
 
-    # Instantiate global Best_Model
-    global _best_model
-
-    # Suggest different learning rates
-    lr = trial.suggest_loguniform('lr', 1e-6, 1e-2)
-
-    # Instantiate the LogisticHazard model
-    LogHazard_Model = LogisticHazard(
-        net = Define_Model(trial)
-        , optimizer = tt.optim.Adam(lr = lr)
-        , duration_index = Label_Transform.cuts
-        , device = Device
+    # Instantiate the PyTorch Lightning Trainer
+    PL_Trainer = pl.Trainer(
+        logger=False
+        , checkpoint_callback=TrialCheckpointCallback
+        , max_epochs=250
+        , gpus=[Device]
+        , callbacks=[Metrics_Callback]
+        , early_stop_callback=optuna.integration.PyTorchLightningPruningCallback(trial, monitor='avg_val_loss')
+        , progress_bar_refresh_rate=0
     )
 
-    # Train the model
-    LogHazard_Model_log = LogHazard_Model.fit(
-        input = x_train
-        , target = y_train
-        , batch_size = 40
-        , epochs = 200
-        , callbacks = [
-            tt.callbacks.EarlyStopping()
-            , Callback_Prune(trial)
-        ]
-        , verbose = False
-        , val_data = (x_valid, y_valid)
+    # Instantiate the PyTorch Lightning module
+    Model = NNet_Lightning(trial)
+
+    # Use Trainer to fit the model
+    PL_Trainer.fit(
+        model=Model
+        , train_dataloader=train_dl
+        , val_dataloaders=valid_dl
     )
 
-    # Store Model
-    _best_model = LogHazard_Model
+    # Return the average validation loss
+    return Metrics_Callback.metrics[-1]['avg_val_loss'].item()
 
-    # Store the minimum validation loss
-    Val_Loss_Min = min(LogHazard_Model.val_metrics.scores['loss']['score'])
-
-    # Return the validation loss
-    return Val_Loss_Min
-
-# TODO: Find cleaner way of storing best trained model during study
 
 # Use SQLAlchemy to instantiate a RDB to store results
-Study_DB = create_engine('sqlite:///Survival Analysis Studies/20200827_LogHazard_Study.db')
-
-# Define callback to save the best_model
-def BestModelCallback(study: optuna.study.Study, trial: optuna.trial.Trial):
-    global Best_Model
-    # Boolean check to see if current trial is the best performing
-    if study.best_trial.number == trial.number:
-        # Save the current model if best performing
-        _best_model.save_net(
-            path = 'Survival Analysis Studies//20200827_LogHazard_Model_{}.sav'.format(trial.number)
-        )
-        Best_Model = _best_model
+Study_DB = create_engine('sqlite:///Classifier Studies/20200929_Classifier_Study.db')
 
 # Run the optimization | no pruning since model.fit function bypasses individual steps
 if __name__ == "__main__":
     # Instantiate the study
-    LogHazard_Study = optuna.create_study(
-        study_name = 'LogisticHazard'
+    Classifier_Study = optuna.create_study(
+        study_name = 'Classifier'
         , direction = 'minimize'
         , sampler = optuna.samplers.TPESampler(
             seed = Random_Seed
@@ -520,93 +509,34 @@ if __name__ == "__main__":
             , n_warmup_steps = 20
             , interval_steps = 5
         )
-        , storage = 'sqlite:///Survival Analysis Studies/20200827_LogHazard_Study.db'
+        , storage = 'sqlite:///Classifier Studies/20200930_Classifier_Study.db'
         , load_if_exists = True
     )
     # Start the optimization
-    LogHazard_Study.optimize(
-        LogHazard_Objective
+    Classifier_Study.optimize(
+        NNet_Objective
         , n_trials = 50
         , n_jobs = Cores
-        , callbacks = [BestModelCallback]
     )
 
-    # Save best generated model
-    LogHazard_Model_Best = Best_Model
-    # LogHazard_Model_best_net = LogHazard_Study.user_attrs['best_model_net']
-    # LogHazard_Model_best_weights = LogHazard_Study.user_attrs['best_model_weights']
-
     # Store the pruned and complete trials
-    pruned_trials = [t for t in LogHazard_Study.trials if t.state == optuna.trial.TrialState.PRUNED]
-    complete_trials = [t for t in LogHazard_Study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+    pruned_trials = [t for t in Classifier_Study.trials if t.state == optuna.trial.TrialState.PRUNED]
+    complete_trials = [t for t in Classifier_Study.trials if t.state == optuna.trial.TrialState.COMPLETE]
 
     # Print statistics
     print("Study statistics: ")
-    print("  Number of finished trials: ", len(LogHazard_Study.trials))
+    print("  Number of finished trials: ", len(Classifier_Study.trials))
     print("  Number of pruned trials: ", len(pruned_trials))
     print("  Number of complete trials: ", len(complete_trials))
 
     # Store best_trial information and print it
-    Best_Trial = LogHazard_Study.best_trial
+    Best_Trial = Classifier_Study.best_trial
     print("Best trial:")
     print("  Value: ", Best_Trial.value)
     print("  Params: ")
     for key, value in Best_Trial.params.items():
         print("    {}: {}".format(key, value))
 
-# Best Valid_Loss = 3.807277
-
-# Pickle the LogHazard_Study | no longer necessary as Study is stored in Sqlite3 DB
-# pickle.dump(
-#     obj = LogHazard_Study
-#     , file = open('Survival Analysis Studies//20200824_LogHazard_Study.sav', 'wb')
-# )
-
-
-# <editor-fold desc="Access LogHazard_Study.db and evaluate key tables">
-
-# Connect to db
-SQLite_DB = create_engine('sqlite:///Survival Analysis Studies/20200827_LogHazard_Study.db')
-
-# Instantiate MetaData object
-DB_MetaData = MetaData(bind = SQLite_DB)
-
-# Automap the existing tables
-DB_MetaData.reflect()
-
-# Create factory for session objects bound to db
-Session = sessionmaker(bind = SQLite_DB)
-
-# Create session object
-session = Session()
-
-# Display tables
-print(DB_MetaData.tables.keys())
-
-# Convert Trials to schema table
-Trials = Table(
-    'trials'
-    , DB_MetaData
-    , autoload = True
-    , autoload_with = SQLite_DB
-)
-
-# Convert Trial_Params to schema table
-Trial_Params = Table(
-    'trial_params'
-    , DB_MetaData
-    , autoload = True
-    , autoload_with = SQLite_DB
-)
-
-# Convert Trials into dataframe
-Trials_df = pd.read_sql(
-    sql = session.query(Trials).statement
-    , con = session.bind
-)
-
-# </editor-fold>
-
-# TESTING SECTION | Commenting out
+# Best Avg Valid_Loss = ?
 
 # </editor-fold>
